074bfc7e435aa2ce8f98c4d6fa105ae3d94e834d
anteroserrano99 <serranoantero13@gmail.com>
2022-08-29 19:15:30 +0200

    Fix default domain name

--- src/main/java/com/utils/UrlCreationUtils.java
+++ src/main/java/com/utils/UrlCreationUtils.java
@@ -9,7 +9,7 @@ public class UrlCreationUtils {
   public final static String ALPHANUMERIC = "0123456789abcdefghijklmnopqrstuvwxyz";
 
 
-  public final static String DEFAULT_DOMAIN = "0123456789abcdefghijklmnopqrstuvwxyz";
+  public final static String DEFAULT_DOMAIN = "http://www.shortDomain.com";
 
   public final static String SLASH = "/";
 
